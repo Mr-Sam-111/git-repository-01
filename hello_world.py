5b501c749cf2803a2c470d83f33ce636b2ee3c10
Mr.Sam <oxxy@i.ua>
2015-09-17 02:19:24 +0300

    add smiles (hello_world)

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 ﻿# -*- coding: utf-8 -*-
 # python 2.7.9
 
-print 'Hello world!!!'
-print 'Hello my dear friend.'
-print 'I love PYTHON.'
+print 'Hello world !!!'
+print 'Hello my dear friend :)'
+print 'I love PYTHON <3'
 print 'And you?'
